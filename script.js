aafaf654006f92aa6fd73afb3fa2ebd5d5e821b5
Martina Hytychová <martina.hyt@gmail.com>
2021-02-27 10:39:47 +0100

    styly pro formulář

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 'use strict';
 console.log('mňau');
-
-window.onload=function(){
-  const omg = document.getElementById(“omg”);
-  console.log(omg)
-  omg.addEventListener(“click”, showForm());
-
-  function showForm() {
-      document.getElementById(“form”).classList.add(“form-show”);
-  }
