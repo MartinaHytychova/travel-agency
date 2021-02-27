58d63163ea936ae1d9c69623c919aaac2d6e4c5c
Martina Hytychová <martina.hyt@gmail.com>
2021-02-27 14:33:58 +0100

    mobile menu

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 'use strict';
 console.log('mňau');
+
+window.onload = function () {
+  const earth = document.getElementById('launchEarth');
+  const moon = document.getElementById('launchMoon');
+  const mars = document.getElementById('launchMars');
+
+  //Displaying the form
+  function showForm() {
+    const orderForm = document.getElementById('form');
+
+    orderForm.style.display = 'block';
+  }
+
+  //Hiding the form
+  const close = document.getElementById('close');
+
+  function hideForm() {
+    const orderForm = document.getElementById('form');
+
+    orderForm.style.display = 'none';
+  }
+
+  earth.addEventListener('click', showForm);
+  moon.addEventListener('click', showForm);
+  mars.addEventListener('click', showForm);
+  close.addEventListener('click', hideForm);
+};
+
+const items = document.querySelector('#items');
+const menu = document.querySelector('.hamburger');
+menu.addEventListener('click', (e) => {
+  menu.classList.toggle('open');
+  items.classList.toggle('opened-list');
+});
